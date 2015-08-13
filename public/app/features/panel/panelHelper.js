da3095775f26cf7f305eb55b55f6bc792571b5d4
Jan Votava <votava@deployment.cz>
2015-08-13 12:54:23 +0200

    Code style

--- public/app/features/panel/panelHelper.js
+++ public/app/features/panel/panelHelper.js
@@ -93,7 +93,7 @@ function (angular, _, kbn, $, moment) {
       function compileDate(date) {
         var string = date.replace(/today\((-?\d*)\)/gi, function(match, num) {
           return moment().add(num, 'day').format('YYYY-MM-DD');
-        })
+        });
 
         return new Date(string);
       }
@@ -101,7 +101,7 @@ function (angular, _, kbn, $, moment) {
       var range = scope.panel.range;
 
       if (range) {
-        var timeInfo = []
+        var timeInfo = [];
 
         if (range.from) {
           scope.range.from = compileDate(range.from);
